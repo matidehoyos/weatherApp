1590ecd010dbd34f1b82615fa9777fe46590bc6f
Matias <mati.dehoyosmdp@gmail.com>
2024-05-23 15:59:20 -0300

    }

--- back/app.js
+++ back/app.js
@@ -10,7 +10,7 @@ app.use(bodyParse.json());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://climappmdh.vercel.app/');
     res.header('Access-Control-Allow-Headers', 'Content-Type'); 
     res.header('Access-Control-Allow-Credentials', true); 
     next();
