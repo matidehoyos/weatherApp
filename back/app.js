6b5ced8946f55c6ba0d09aedc1042e5ded63b584
Matias <mati.dehoyosmdp@gmail.com>
2024-05-23 16:14:50 -0300

    }

--- back/app.js
+++ back/app.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const bodyParse = require('body-parser')
-const morgan = require("morgan");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
-const routes = require('./routes');
+const routes = require('./routes'); 
 
-app.use(bodyParse.urlencoded({extended: true}));
-app.use(bodyParse.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(morgan("dev"));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://climappmdh.vercel.app/');
-    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
-    res.header('Access-Control-Allow-Credentials', true); 
-    next();
-});
+app.use(morgan('dev'));
+
 
 app.use(cors());
-app.use(routes);
 
+app.use(routes);
 
 app.listen(3001, () => {
     console.log('Servidor iniciado en el puerto 3001');
